c091a18e1e9dea0d7ff48a3eb39b069b41b2a31f
fareehaabbasi <abbasifareeha9@gmail.com>
2025-12-17 02:42:30 +0500

    add-pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Link, Route, Routes } from 'react-router-dom'
+import Home from './pages/home'
+import Projects from './pages/Projects'
+import Agency from './pages/Agency'
 
 const App = () => {
   return (
     <div>
-      
+      <Link to='/'>Home</Link>
+      <Link to='/projects'>Projects</Link>
+      <Link to='/agency'>Agency</Link>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/projects' element={<Projects />}></Route>
+        <Route path='/agency' element={<Agency />}></Route>
+      </Routes>
     </div>
   )
 }
---
+++ src/pages/Agency.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Agency = () => {
+  return (
+    <div>
+      <h1>Hello, Agency Page</h1>
+    </div>
+  )
+}
+
+export default Agency
